86b7ce77f6d97c73b4277d334886eb2612f60990
Vadim Zhuravlev <vadim.zhuravlev.cranewf@gmail.com>
2024-10-15 11:13:41 +0300

    Реализовано задание base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,44 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let discriminant = b ** 2 - 4 * a * c;
+
+	if (discriminant < 0) {
+		return [];
+	}
+
+	if (discriminant === 0) {
+		let root = -b / (2 * a);
+		return [root];
+	}
+
+	if (discriminant > 0) {
+		let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		return [root1, root2];
+	}
+
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+	percent = parseFloat(percent);
+	contribution = parseFloat(contribution);
+	amount = parseFloat(amount);
+	countMonths = parseFloat(countMonths);
+
+	if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+		return false;
+	}
+
+	let rateConversion = (percent / 100) / 12;
+
+	let loanBody = amount - contribution;
+
+	let monthlyPayment = loanBody * (rateConversion + (rateConversion / (((1 + rateConversion) ** countMonths) - 1)));
+
+	let totalAmount = rateConversion * countMonths + contribution;
+
+	return Number(totalAmount.toFixed(2));
+
 }
