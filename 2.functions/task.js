471cad9f8b2eeeff1554ba1d05f2a3681e1d2904
Vadim Zhuravlev <vadim.zhuravlev.cranewf@gmail.com>
2024-10-16 11:48:22 +0300

    Реализовано задание по теме Функции

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,87 @@
 function getArrayParams(...arr) {
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
 
-  return { min: min, max: max, avg: avg };
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		sum += arr[i];
+	}
+	let avg = Number((sum / arr.length).toFixed(2));
+
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
 }
 
 function summElementsWorker(...arr) {
+	let sum = 0;
 
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
-
+	if (arr.length === 0) {
+		return 0;
+	}
+	const max = Math.max(...arr);
+	const min = Math.min(...arr);
+	return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
+	let sumEvenElement = 0;
+	let sumOddElement = 0;
 
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+		} else {
+			sumOddElement += arr[i];
+		}
+	}
+	return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
 
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+			countEvenElement++;
+		}
+	}
+
+	if (countEvenElement > 0) {
+		return sumEvenElement / countEvenElement;
+	} else {
+		return 0;
+	}
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
 
+	let maxWorkerResult = -Infinity;
+
+	for (let i = 0; i < arrOfArr.length; i++) {
+		const elementToBeIterated = func(...arrOfArr[i]);
+
+		if (elementToBeIterated > maxWorkerResult) {
+			maxWorkerResult = elementToBeIterated;
+		}
+
+	}
+	return maxWorkerResult;
 }
