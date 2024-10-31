0a969695127e208f966a7e2b2a974a853512a9a1
Vadim Zhuravlev <vadim.zhuravlev.cranewf@gmail.com>
2024-10-31 13:31:57 +0300

    Реализовано задание по объектам

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
-}
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
+	this.subject = null;
+	this.excluded = null;
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.setSubject = function(subjectName) {
+	if (this.excluded === null) {
+		this.subject = subjectName;
+	}
+};
 
-Student.prototype.getAverage = function () {
-  
-}
+Student.prototype.addMarks = function(...marks) {
+	if (this.excluded === null) {
+		this.marks.push(...marks);
+	}
+};
 
-Student.prototype.exclude = function (reason) {
-  
-}
+Student.prototype.getAverage = function() {
+	if (!this.marks || this.marks.length === 0) {
+		return 0;
+	}
+	const total = this.marks.reduce((sum, mark) => sum + mark, 0);
+	return total / this.marks.length;
+};
+
+Student.prototype.exclude = function(reason) {
+	this.excluded = reason;
+	this.subject = undefined;
+	this.marks = undefined;
+};
